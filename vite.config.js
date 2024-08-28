a3575a8b0e0c91b7b254e528c46858ea14924f9e
Nicolas97431 <nickogsm@gmail.com>
2024-08-28 16:08:08 +0400

    2eme modif

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/AquaCalcul/", // Remplace par le nom de ton dépôt
   plugins: [react()],
-})
+});
