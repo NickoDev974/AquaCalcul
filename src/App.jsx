6c9086b596e76ea0cb205d01f361227d5d8d36df
Nicolas97431 <nickogsm@gmail.com>
2024-08-29 07:50:43 +0400

    css modif 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import { useState } from "react";
 import "./App.css";
 
+//image import
+import aquaCube from "./assets/picture/aqua-cube.jpg";
+import cilinderCube from "./assets/picture/volume_cylindre.png";
+import medoc from "./assets/picture/calculs-medocs.jpg";
+import bache from "./assets/picture/bache-bassin.jpg";
+import cemaqua from "./assets/picture/cemaqua.png";
+
 function App() {
   // États pour le calcul du volume cubique
   const [cubeLength, setCubeLength] = useState(0);
@@ -72,17 +79,23 @@ function App() {
 
   return (
     <>
-      <h1>Aqua Calcul</h1>
-      <p>Tout pour les calculs dans l'aquariophilie</p>
-
+      <section className="title">
+        <h1>Aqua Calcul</h1>
+        <p>Tout pour les calculs dans l'aquariophilie</p>
+      </section>
       {/* Section calcul du volume */}
       <section className="volume">
         <h2>Calcul de volume</h2>
-        <p>Combien de litres dans mon aquarium ou bassin ?</p>
+        <p>Combien de litres dans mon aquarium ou mon bassin ?</p>
 
         {/* Volume d'un aquarium cubique */}
         <article>
           <h3>Volume d'un Cube</h3>
+          <img
+            src={aquaCube}
+            alt="volume aquarium pavé"
+            className="imgSection"
+          />
           <label htmlFor="cubeLength">Longueur (cm)</label>
           <input
             id="cubeLength"
@@ -107,13 +120,19 @@ function App() {
           <button onClick={calculateCubeVolume}>Calculer le volume</button>
           {volumeCube && (
             <p>
-              Le volume de votre aquarium cubique est de {volumeCube} litres.
+              Le volume de votre aquarium cubique est de{" "}
+              <span>{volumeCube} litres</span>.
             </p>
           )}
         </article>
         <article>
           {/* Volume d'un aquarium cylindrique */}
           <h3>Volume d'un Cylindre</h3>
+          <img
+            src={cilinderCube}
+            alt="volume aquarium cylindre"
+            className="imgSection"
+          />
           <label htmlFor="radius">Rayon (cm)</label>
           <input
             id="radius"
@@ -131,129 +150,146 @@ function App() {
           <button onClick={calculateCylinderVolume}>Calculer le volume</button>
           {volumeCylinder && (
             <p>
-              Le volume de votre aquarium cylindrique est de {volumeCylinder}{" "}
-              litres.
+              Le volume de votre aquarium cylindrique est de{" "}
+              <span>{volumeCylinder} litres</span>.
             </p>
           )}
         </article>
       </section>
       {/* Section calcul du dosage de traitement */}
-      <h2>Dosage de traitement</h2>
-      <p>
-        Calculez la quantité de traitement nécessaire en fonction du volume
-        d'eau.
-      </p>
+      <section>
+        <h2>Dosage de traitement</h2>
+        <p>
+          Calculez la quantité de traitement nécessaire en fonction du volume
+          d'eau.
+        </p>
+        <img src={medoc} alt="calcul medication" className="imgSection" />
 
-      <label htmlFor="treatmentQuantity">Quantité de traitement (ml)</label>
-      <input
-        id="treatmentQuantity"
-        type="number"
-        value={treatmentQuantity}
-        onChange={(e) => setTreatmentQuantity(e.target.value)}
-      />
-      <label htmlFor="treatmentVolume">
-        Volume d'eau pour cette quantité (L)
-      </label>
-      <input
-        id="treatmentVolume"
-        type="number"
-        value={treatmentVolume}
-        onChange={(e) => setTreatmentVolume(e.target.value)}
-      />
-      <label htmlFor="waterVolume">Volume d'eau à traiter (L)</label>
-      <input
-        id="waterVolume"
-        type="number"
-        value={waterVolume}
-        onChange={(e) => setWaterVolume(e.target.value)}
-      />
-      <button onClick={calculateTreatment}>Calculer le dosage</button>
-      {requiredTreatment && (
-        <p>Vous avez besoin de {requiredTreatment} ml de traitement.</p>
-      )}
-
+        <label htmlFor="treatmentQuantity">Quantité de traitement (ml)</label>
+        <input
+          id="treatmentQuantity"
+          type="number"
+          value={treatmentQuantity}
+          onChange={(e) => setTreatmentQuantity(e.target.value)}
+        />
+        <label htmlFor="treatmentVolume">
+          Volume d'eau pour cette quantité (L)
+        </label>
+        <input
+          id="treatmentVolume"
+          type="number"
+          value={treatmentVolume}
+          onChange={(e) => setTreatmentVolume(e.target.value)}
+        />
+        <label htmlFor="waterVolume">Volume d'eau à traiter (L)</label>
+        <input
+          id="waterVolume"
+          type="number"
+          value={waterVolume}
+          onChange={(e) => setWaterVolume(e.target.value)}
+        />
+        <button onClick={calculateTreatment}>Calculer le dosage</button>
+        {requiredTreatment && (
+          <p>
+            Vous avez besoin de <span>{requiredTreatment} ml </span>de
+            traitement.
+          </p>
+        )}
+      </section>
       {/* Section calcul de la bâche */}
-      <h2>Calcul de bâche</h2>
-      <p>Calcul de la taille de la bâche nécessaire pour un bassin cubique.</p>
-
-      <label htmlFor="bacheLength">Longueur (m)</label>
-      <input
-        id="bacheLength"
-        type="number"
-        value={bacheLength}
-        onChange={(e) => setBacheLength(e.target.value)}
-      />
-      <label htmlFor="bacheWidth">Largeur (m)</label>
-      <input
-        id="bacheWidth"
-        type="number"
-        value={bacheWidth}
-        onChange={(e) => setBacheWidth(e.target.value)}
-      />
-      <label htmlFor="bacheHeight">Hauteur (m)</label>
-      <input
-        id="bacheHeight"
-        type="number"
-        value={bacheHeight}
-        onChange={(e) => setBacheHeight(e.target.value)}
-      />
-      <button onClick={calculateBache}>Calculer la taille de la bâche</button>
-      {/* Affichage des dimensions réelles calculées */}
-      {bacheLength && bacheWidth && bacheHeight && (
+      <section>
+        <h2>Calcul de bâche</h2>
         <p>
-          La taille réelle de la bâche nécessaire est de{" "}
-          {(parseFloat(bacheLength) + 1 + parseFloat(bacheHeight) * 2).toFixed(
-            2
-          )}{" "}
-          m de longueur et{" "}
-          {(parseFloat(bacheWidth) + 1 + parseFloat(bacheHeight) * 2).toFixed(
-            2
-          )}{" "}
-          m de largeur.
+          Calcul de la taille de la bâche nécessaire pour un bassin cubique.
         </p>
-      )}
+        <img src={bache} alt="pose de bache" className="imgSection" />
 
-      {/* Affichage de la coupe à réaliser */}
-      {bacheRealLength && bacheRealWidth && (
-        <p>
-          Vous devrez couper une bâche de {bacheRealLength} m de longueur sur
-          une largeur de {bacheRealWidth} m (en fonction des largeurs
-          disponibles : 4m, 6m, 8m, 10m).
-        </p>
-      )}
+        <label htmlFor="bacheLength">Longueur (m)</label>
+        <input
+          id="bacheLength"
+          type="number"
+          value={bacheLength}
+          onChange={(e) => setBacheLength(e.target.value)}
+        />
+        <label htmlFor="bacheWidth">Largeur (m)</label>
+        <input
+          id="bacheWidth"
+          type="number"
+          value={bacheWidth}
+          onChange={(e) => setBacheWidth(e.target.value)}
+        />
+        <label htmlFor="bacheHeight">Hauteur (m)</label>
+        <input
+          id="bacheHeight"
+          type="number"
+          value={bacheHeight}
+          onChange={(e) => setBacheHeight(e.target.value)}
+        />
+        <button onClick={calculateBache}>Calculer la taille de la bâche</button>
+        {/* Affichage des dimensions réelles calculées */}
+        {bacheLength && bacheWidth && bacheHeight && (
+          <p>
+            La taille réelle de la bâche nécessaire est de{" "}
+            {(
+              parseFloat(bacheLength) +
+              1 +
+              parseFloat(bacheHeight) * 2
+            ).toFixed(2)}{" "}
+            m de longueur et{" "}
+            {(parseFloat(bacheWidth) + 1 + parseFloat(bacheHeight) * 2).toFixed(
+              2
+            )}{" "}
+            m de largeur.
+          </p>
+        )}
 
+        {/* Affichage de la coupe à réaliser */}
+
+        {bacheRealLength && bacheRealWidth && (
+          <p>
+            Vous devrez couper une bâche de <span>{bacheRealLength} m</span> de
+            longueur sur une largeur de <span>{bacheRealWidth} m </span>
+            (en fonction des largeurs disponibles : 4m, 6m, 8m, 10m).
+          </p>
+        )}
+      </section>
       {/* Section calcul de la surface à peindre */}
-      <h2>Calcul de la surface à peindre</h2>
-      <p>Calculez la surface des 5 faces à peindre d'un bassin cubique.</p>
+      <section>
+        <h2>Calcul de la surface à peindre</h2>
+        <p>Calculez la surface des 5 faces à peindre d'un bassin cubique.</p>
+        <img src={cemaqua} alt="Pose de cemaqua" className="imgSection" />
 
-      <label htmlFor="paintLength">Longueur (m)</label>
-      <input
-        id="paintLength"
-        type="number"
-        value={paintLength}
-        onChange={(e) => setPaintLength(e.target.value)}
-      />
-      <label htmlFor="paintWidth">Largeur (m)</label>
-      <input
-        id="paintWidth"
-        type="number"
-        value={paintWidth}
-        onChange={(e) => setPaintWidth(e.target.value)}
-      />
-      <label htmlFor="paintHeight">Hauteur (m)</label>
-      <input
-        id="paintHeight"
-        type="number"
-        value={paintHeight}
-        onChange={(e) => setPaintHeight(e.target.value)}
-      />
-      <button onClick={calculateSurfaceToPaint}>
-        Calculer la surface à peindre
-      </button>
+        <label htmlFor="paintLength">Longueur (m)</label>
+        <input
+          id="paintLength"
+          type="number"
+          value={paintLength}
+          onChange={(e) => setPaintLength(e.target.value)}
+        />
+        <label htmlFor="paintWidth">Largeur (m)</label>
+        <input
+          id="paintWidth"
+          type="number"
+          value={paintWidth}
+          onChange={(e) => setPaintWidth(e.target.value)}
+        />
+        <label htmlFor="paintHeight">Hauteur (m)</label>
+        <input
+          id="paintHeight"
+          type="number"
+          value={paintHeight}
+          onChange={(e) => setPaintHeight(e.target.value)}
+        />
+        <button onClick={calculateSurfaceToPaint}>
+          Calculer la surface à peindre
+        </button>
 
-      {surfaceToPaint && (
-        <p>La surface totale à peindre est de {surfaceToPaint} m².</p>
-      )}
+        {surfaceToPaint && (
+          <p>
+            La surface totale à peindre est de <span>{surfaceToPaint} m²</span>.
+          </p>
+        )}
+      </section>
     </>
   );
 }
